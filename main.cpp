bee89964e6b5d2ef8cac92fe40d0361ebb4b6df7
Ahmed Talsi <149481952+Mr-Talsi@users.noreply.github.com>
2024-09-12 15:43:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,17 +12,27 @@ int main()
   tictatoe board;
   int starter;
   int x;
+  int niveau;
   string mode;
 
   cout << "bienvenue au jeu \n";
   cout<<"choissir un mode --> normal ou robot :";
   cin>>mode;
+
+  
+  if (mode=="robot")
+  {
+  cout<<"\n1-facile \n2-intermediare\n3-extreme\nchoissir un niveau -->";
+  cin>>niveau;
+  }
+  
+
   cout << "choissir X/O : 1/2 ?";
   cin >> starter;
 
   check(starter);
   board.SetTurn(starter);
-
+  board.setniveau(niveau);
   while (board.HaveWinner() == 0)
   { 
     board.mode(mode,x);
